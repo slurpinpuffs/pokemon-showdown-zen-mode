dfac0be621bf137a481de32a4f2a26453962ee53
slurpinpuffs <swirlinlix@gmail.com>
2024-09-10 01:11:21 -0700

    Tweaked Ledyba + Ledian

--- config/formats.ts
+++ config/formats.ts
@@ -7,13 +7,12 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		name: "[Gen 3] Zen Mode Singles",
 		mod: 'gen3',
 		ruleset: ['Standard', 'One Boost Passer Clause', 'Freeze Clause Mod'],
-		banlist: ['Uber', 'Smeargle + Ingrain', 'Sand Veil', 'Soundproof', 'Assist', 'Baton Pass + Block', 'Baton Pass + Mean Look', 'Baton Pass + Spider Web', 'Swagger'],
-		unbanlist: ['Paras + Immunity', 'Parasect + Immunity'],
+		banlist: ['Uber', 'OU', 'Smeargle + Ingrain', 'Sand Veil', 'Soundproof', 'Assist', 'Baton Pass + Block', 'Baton Pass + Mean Look', 'Baton Pass + Spider Web', 'Swagger'],
 	},
 	{
 		name: "[Gen 4] Zen Mode Singles",
 		mod: 'gen4',
 		ruleset: ['Standard'],
-		banlist: ['AG', 'Uber', 'Arena Trap', 'Quick Claw', 'Soul Dew', 'Swagger'],
+		banlist: ['AG', 'Uber', 'OU', 'Arena Trap', 'Quick Claw', 'Soul Dew', 'Swagger'],
 	},
 ];
